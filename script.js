74f719c8ce83d7c746a0dbb902e963bd0c4b7878
Ravneet04 <121482471+Ravneet04@users.noreply.github.com>
2023-08-28 18:16:28 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function calculator(newvalue){
+    document.getElementById('myResult').value += newvalue;
+}
+
+function answer(){
+    var a = document.getElementById('myResult').value;
+    var b = eval(a);
+    document.getElementById('myResult').value = b;
+}
+function DeleteMe(){
+    document.getElementById('myResult').value = ''
+}
